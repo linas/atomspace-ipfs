2552ee39f55c3a96819d5fd20ba65adb22663cac
Linas Vepstas <linasvepstas@gmail.com>
2019-10-17 20:55:21 -0500

    Create a file ...

--- opencog/persist/ipfs/sniff.cc
+++ opencog/persist/ipfs/sniff.cc
@@ -11,7 +11,22 @@ int main (int, const char **)
 {
 	std::stringstream contents;
 	ipfs::Client client("localhost", 5001);
+#if 0
 	client.FilesGet("/ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme", &contents);
 	std::cout << contents.str() << std::endl;
+#endif
+
+	ipfs::Json version;
+	client.Version(&version);
+	std::cout << "Peer's version: " << version << std::endl;
+
+	ipfs::Json add_result;
+	client.FilesAdd(
+		{{"foo.txt", ipfs::http::FileUpload::Type::kFileContents,
+			"(Concept \"abcd\")"},},
+		&add_result);
+	std::cout << "FilesAdd() result:" << std::endl
+			  << add_result.dump(2) << std::endl;
+
 	return 0;
 }
