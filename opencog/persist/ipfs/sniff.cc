7fda2b27e9fb7b239403eb649c37f69155d8fcf6
Linas Vepstas <linasvepstas@gmail.com>
2019-10-17 22:40:14 -0500

    start adding concepts

--- opencog/persist/ipfs/sniff.cc
+++ opencog/persist/ipfs/sniff.cc
@@ -7,53 +7,28 @@
 
 #include <ipfs/client.h>
 
+std::string addAtom(ipfs::Client& client, const std::string& scm_text)
+{
+	ipfs::Json add_result;
+	client.FilesAdd({
+		{"Atom.scm",
+			ipfs::http::FileUpload::Type::kFileContents,
+			scm_text,
+		},},
+		&add_result);
+	std::cout << "addAtom result:" << std::endl
+			  << add_result.dump(2) << std::endl;
+	return add_result[0]["hash"];
+}
+
 int main (int, const char **)
 {
 	std::stringstream contents;
 	ipfs::Client client("localhost", 5001);
-#if 0
-	client.FilesGet("/ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme", &contents);
-	std::cout << contents.str() << std::endl;
-#endif
 
-std::string cona =
-({
-	ipfs::Json add_result;
-	client.FilesAdd({
-		{"Concept.scm",
-			ipfs::http::FileUpload::Type::kFileContents,
-			"(Concept \"abcd\")\n"},
-		},
-		&add_result);
-	std::cout << "FilesAdd(abcd) result:" << std::endl
-			  << add_result.dump(2) << std::endl;
-	add_result[0]["hash"];
-});
-	std::string conp =
-({
-	ipfs::Json add_result;
-	client.FilesAdd({
-		{"Concept.scm",
-			ipfs::http::FileUpload::Type::kFileContents,
-			"(Concept \"pqrs\")\n"},
-		},
-		&add_result);
-	std::cout << "FilesAdd(pqrs) result:" << std::endl
-			  << add_result.dump(2) << std::endl;
-	add_result[0]["hash"];
-});
-{
-	ipfs::Json add_result;
-	client.FilesAdd({
-		{"Predicate.scm",
-			ipfs::http::FileUpload::Type::kFileContents,
-			"(Predicate \"p\")\n"},
-		},
-		&add_result);
-	std::cout << "FilesAdd(pred) result:" << std::endl
-			  << add_result.dump(2) << std::endl;
-	std::string pred = add_result[0]["hash"];
-}
+	std::string cona = addAtom(client, "(Concept \"abcd\")\n");
+	std::string conp = addAtom(client, "(Concept \"pqrs\")\n");
+	std::string pred = addAtom(client, "(Predicate \"p\")\n");
 
 	std::string data;
 	client.ObjectData(cona, &data);
