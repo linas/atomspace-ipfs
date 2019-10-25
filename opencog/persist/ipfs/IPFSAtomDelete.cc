d97c0d3e663e47c9e359c7e93bb9c07252c99a8c
Linas Vepstas <linasvepstas@gmail.com>
2019-10-25 14:15:03 -0500

    Implement atom removal

--- opencog/persist/ipfs/IPFSAtomDelete.cc
+++ opencog/persist/ipfs/IPFSAtomDelete.cc
@@ -24,17 +24,47 @@ using namespace opencog;
 /// unless the recursive flag is set. If the recursive flag is set, then
 /// the atom, and everything in its incoming set is removed.
 ///
-/// TODO The deletion is performed synchronously, in single-threaded
-/// fashion.  It might make sense to create a queue for this, so that
-/// if could run in parallel (in the unusual case that someone has
-/// millions of atoms to delete and is impateint about it...)
 void IPFSAtomStorage::removeAtom(const Handle& h, bool recursive)
 {
 	// Synchronize. The atom that we are deleting might be sitting
 	// in the store queue.
 	flushStoreQueue();
 
-	throw SyntaxException(TRACE_INFO, "Not Implemented!\n");
+	// First, look it up.
+	std::string name = h->to_short_string();
+	std::string path = _atomspace_cid + "/" + name;
+	ipfs::Json dag;
+	ipfs::Client* conn = conn_pool.pop();
+	conn->DagGet(path, &dag);
+	conn_pool.push(conn);
+
+	auto iset = dag["incoming"];
+
+	// Fail if a non-trivial incoming set.
+	if (not recursive and 0 < iset.size()) return;
+
+	// We're recursive; so recurse.
+	for (auto acid: iset)
+	{
+		removeAtom(fetch_atom(acid), true);
+	}
+
+	// Now actually remove.
+	std::string new_as_id;
+	conn = conn_pool.pop();
+	{
+		// Update the cid under a lock, as atomspace modifications
+		// can occur from multiple threads.  It's not actually the
+		// cid that matters, its the patch itself.
+		std::lock_guard<std::mutex> lck(_atomspace_cid_mutex);
+		conn->ObjectPatchRmLink(_atomspace_cid, name, &new_as_id);
+		_atomspace_cid = new_as_id;
+		std::cout << "Atomspace after removal of " << name
+		          << " is " << _atomspace_cid << std::endl;
+	}
+	conn_pool.push(conn);
+
+	// Bug with stats: should not increment on recursion.
 	_num_atom_deletes++;
 	_num_atom_removes++;
 }
