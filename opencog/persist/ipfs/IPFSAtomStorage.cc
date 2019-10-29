e9468f49c0a057de885366dd45ff523ec8afa0d8
Linas Vepstas <linasvepstas@gmail.com>
2019-10-28 22:47:34 -0500

    Bug-fix what get_json does

--- opencog/persist/ipfs/IPFSAtomStorage.cc
+++ opencog/persist/ipfs/IPFSAtomStorage.cc
@@ -235,28 +235,27 @@ std::string IPFSAtomStorage::get_atom_guid(const Handle& h)
 /**
  * Return the IPFS json of this Atom, including the current state
  * for Values and for the incoming set.
- *
- * XXX FIXME. This ios broken/racey. Two different threads could
- * ask for the json, and then edit it, and then update the _json_map
- * with the edited version, thus clobbering one-another, as the second
- * writer wins.  We really need an atomic get-and-update for the
- * _json_map contents.  This is (I think) the reason why DeleteUTest
- * fails -- atom values and incoming are clobbering one-another.
  */
 ipfs::Json IPFSAtomStorage::get_atom_json(const Handle& atom)
 {
-	// First, see if we have a cache of it.
-	{
-		std::lock_guard<std::mutex> lck(_json_mutex);
-		auto pj = _json_map.find(atom);
-		if (_json_map.end() != pj) return pj->second;
-	}
+	// Build the name
+	std::string path = _atomspace_cid + "/" + atom->to_short_string();
 
-	// Not found in the cache. Build it from thin air.
-	// XXX FIXME In a proper decentralized system, we'd ask IPFS for it,
-	// because some other peer may have put it in IPFS. But we're
-	// centralized, right now, so just create it.
-	return encodeAtomToJSON(atom);
+	// std::cout << "Query path = " << path << std::endl;
+	ipfs::Json dag;
+	ipfs::Client* conn = conn_pool.pop();
+	try
+	{
+		conn->DagGet(path, &dag);
+	}
+	catch (const std::exception& ex)
+	{
+		// If we are here, its because there was no such Atom
+		// recorded in IPFS. That's a normal situation, just
+		// ignore the error.
+	}
+	conn_pool.push(conn);
+	return dag;
 }
 
 /**
